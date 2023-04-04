790e6e2158aafec5823040cf7d9bf6f9ede04ccb
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-04 13:34:30 +0900

    type hints

--- put_parquet_gz_to_s3/main.py
+++ put_parquet_gz_to_s3/main.py
@@ -1,13 +1,14 @@
+from typing import Dict, List
+from io import BytesIO
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 import gzip
 import boto3
-from io import BytesIO
 
 
-def create_dummy_data(rows=1000):
-    data = {
+def create_dummy_data(rows: int = 1000) -> pd.DataFrame:
+    data: Dict[str, List] = {
         'id': range(1, rows + 1),
         'name': [f'name_{i}' for i in range(1, rows + 1)],
         'value': [i * 100 for i in range(1, rows + 1)]
@@ -15,29 +16,29 @@ def create_dummy_data(rows=1000):
     return pd.DataFrame(data)
 
 
-def save_gz_parquet(df, file_path):
-    table = pa.Table.from_pandas(df)
-    buffer = BytesIO()
+def save_gz_parquet(df: pd.DataFrame, file_path: str) -> None:
+    table: pa.Table = pa.Table.from_pandas(df)
+    buffer: BytesIO = BytesIO()
     with gzip.open(buffer, 'wb') as gz_file:
         pq.write_table(table, gz_file)
     with open(file_path, 'wb') as file:
         file.write(buffer.getvalue())
 
 
-def upload_to_s3(bucket, s3_key, file_path):
+def upload_to_s3(bucket: str, s3_key: str, file_path: str) -> None:
     s3 = boto3.client('s3')
     with open(file_path, 'rb') as file:
         s3.upload_fileobj(file, bucket, s3_key)
 
 
-def main():
-    dummy_data = create_dummy_data()
+def main() -> None:
+    dummy_data: pd.DataFrame = create_dummy_data()
 
-    file_path = 'dummy_data.parquet.gz'
+    file_path: str = 'dummy_data.parquet.gz'
     save_gz_parquet(dummy_data, file_path)
 
-    bucket_name = 'your-bucket-name'
-    s3_key = 'path/to/dummy_data.parquet.gz'
+    bucket_name: str = 'your-bucket-name'
+    s3_key: str = 'path/to/dummy_data.parquet.gz'
     upload_to_s3(bucket_name, s3_key, file_path)
 
 
